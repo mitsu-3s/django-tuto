0257c0d5d5bcb2699673ae49fc532c5bb9f989f4
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-20 09:27:19 +0900

    Database Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ne+h8_$ri4&f-p--p64nd^4w+ic=!at17-en@1h5h#b6+eluk$"
+SECRET_KEY = (
+    "django-insecure-ne+h8_$ri4&f-p--p64nd^4w+ic=!at17-en@1h5h#b6+eluk$"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",  #'polls/apps.py'のPollsConfigを読み込んで連携
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question, Choice  # データベースモデルの場所(.models)→管理者に伝えるため
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
