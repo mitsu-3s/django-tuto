c8fe2ef7c3e33351b0360a49a5642b169080ed09
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-21 02:54:36 +0900

    Add URL Reverse Resolution

--- polls/urls.py
+++ polls/urls.py
@@ -6,17 +6,14 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path(
-        "hello/", views.hellofunc
-    ),  # /polls/hello/のリクエストを受け取ったら、hellofunc関数を実行
-    path("", views.index, name="index"),  # ex: /polls/
-    path(
-        "<int:question_id>/", views.detail, name="detail"
-    ),  # ex: /polls/5/のリクエストを受け取ったら、views.pyのdetailを実行(question_idで、pathのint:question_idを引数とする)
-    path(
-        "<int:question_id>/results/", views.results, name="results"
-    ),  # ex: /polls/5/results/
-    path(
-        "<int:question_id>/vote/", views.vote, name="vote"
-    ),  # ex: /polls/5/vote/
+    # /polls/hello/のリクエストを受け取ったら、hellofunc関数を実行
+    path("hello/", views.hellofunc),
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/のリクエストを受け取ったら、views.pyのdetailを実行(question_idで、pathのint:question_idを引数とする)
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Question
 
@@ -9,14 +9,45 @@ def hellofunc(request):
     return HttpResponse("Hello")
 
 
+# ショートカット前、render()を使わない
+# def index(request):
+#     # Questionモデルから、pub_dateで降順で並び替えた後に、最初の5つの要素だけを取得する
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+
+#     # templates/polls/index.htmlを読み込む
+#     template = loader.get_template("polls/index.html")
+
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+# ショートカット後
 def index(request):
+    # Questionモデルから、pub_dateで降順で並び替えた後に、最初の5つの要素だけを取得する
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
+    # 第1引数としてrequestオブジェクト、第2引数としてテンプレート名（場所）、第3引数（任意）で辞書
     return render(request, "polls/index.html", context)
 
 
-def detail(request, question_id):  # urls.pyの<int:question_id>を引数としている
-    return HttpResponse("You're looking at question %s." % question_id)
+# ショートカット前
+# def detail(request, question_id):  # urls.pyの<int:question_id>を引数としている
+#     try:
+#         # pkはprimary keyで、Questionモデルからquestion_idに合致するものを取得
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+
+# ショートカット後
+def detail(request, question_id):
+    # オブジェクトがなかったら、Http404を送出
+    question = get_object_or_404(Question, pk=question_id)
+    # 第1引数としてrequestオブジェクト、第2引数としてテンプレート名（場所）、第3引数（任意）で辞書
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
